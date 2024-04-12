6d8a098b0ba5df595dfe71deb77ba2ca4eb9f81e
TuanND25 <nduy.tuan3110@gmail.com>
2024-04-12 16:24:13 +0700

    done

--- TMDT/Services/OrderItemService.cs
+++ TMDT/Services/OrderItemService.cs
@@ -2,67 +2,77 @@
 
 namespace TMDT.Services
 {
-    public class OrderItemService
-    {
-        db_ECommerceShopContext _context;
+	public class OrderItemService
+	{
+		db_ECommerceShopContext _context;
 
-        public OrderItemService()
-        {
-            _context = new db_ECommerceShopContext();
-        }
+		public OrderItemService()
+		{
+			_context = new db_ECommerceShopContext();
+		}
 
-        public List<OrderItem> GetAllOrderItems()
-        {
-            return _context.OrderItems.ToList();
-        }
+		public List<OrderItem> GetAllOrderItems()
+		{
+			return _context.OrderItems.ToList();
+		}
 
-        public OrderItem GetOrderItemByProductId(int productId)
-        {
-            return _context.OrderItems.FirstOrDefault(c => c.ProductId == productId);
-        }
+		public OrderItem GetOrderItemByProductId(int productId)
+		{
+			return _context.OrderItems.FirstOrDefault(c => c.ProductId == productId);
+		}
 
-        public bool AddOrderItem(OrderItem orderItem)
-        {
-            try
-            {
-                _context.OrderItems.Add(orderItem);
-                _context.SaveChanges();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+		public List<OrderItem> GetCartOrderItem(int OrderId)
+		{
+			return _context.OrderItems.Where(c => c.OrderId == OrderId).ToList();
+		}
 
-        public bool UpdateOrderItem(OrderItem orderItem)
-        {
-            try
-            {
-                var oi = GetOrderItemByProductId(orderItem.ProductId);
-                oi.Quantity = orderItem.Quantity;
-                _context.SaveChanges();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+		public bool CheckCart(int idProduct)
+		{
+			return _context.OrderItems.Any(c => c.ProductId == idProduct) ? true : false;
+		}
 
-        public bool DeleteOrderItem(int productId)
-        {
-            try
-            {
-                var oi = GetOrderItemByProductId(productId);
-                _context.OrderItems.Remove(oi);
-                _context.SaveChanges();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-    }
+		public bool AddOrderItem(OrderItem orderItem)
+		{
+			try
+			{
+				_context.OrderItems.Add(orderItem);
+				_context.SaveChanges();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool UpdateOrderItem(OrderItem orderItem)
+		{
+			try
+			{
+				var oi = GetOrderItemByProductId(orderItem.ProductId);
+				oi.Quantity = orderItem.Quantity;
+				_context.SaveChanges();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool DeleteOrderItem(int productId)
+		{
+			try
+			{
+				var oi = GetOrderItemByProductId(productId);
+				_context.OrderItems.Remove(oi);
+				_context.SaveChanges();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
 }
